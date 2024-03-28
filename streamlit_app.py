5746814b67d618278b601f4a9d995101f917927d
sohelkhan121101 <79909435+sohelkhan121101@users.noreply.github.com>
2024-03-28 09:42:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,8 @@ if ingredient_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+# st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
 
 # # Get the current credentials
 # session = get_active_session()
